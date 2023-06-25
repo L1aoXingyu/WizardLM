4b78f9d3d92800b8af9072dc867e04a5185a8f81
xingyu.liao <sherlockliao01@gmail.com>
2023-06-25 16:11:46 +0800

    update human_eval benchmark code

--- WizardCoder/humaneval_postprocess.sh
+++ WizardCoder/humaneval_postprocess.sh
@@ -2,9 +2,9 @@
 set -x
 
 temp=0.2
-pred_num=200
+pred_num=1
 
-output_path=preds/T${temp}_N${pred_num}
+output_path=preds/starcoderbase-code_evol-Greedy_Decode/T${temp}_N${pred_num}
 
 echo 'Output path: '$output_path
 python src/process_humaneval.py --path ${output_path} --out_path ${output_path}.jsonl --add_prompt
